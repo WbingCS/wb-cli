ac7414994862fbdf0cdb375ccf9467ace796e0d3
王冰 <421768544@qq.com>
2019-07-18 21:50:25 +0800

    feat: init
    Change-Id: I9a844bb1cbc92af4ec03a6513ace5c4f83aeaccc

--- utils/logger.js
+++ utils/logger.js
@@ -5,6 +5,10 @@ exports.success = function(msg) {
   console.log(logSymbols.success, chalk.green(msg));
 };
 
+exports.info = function(msg) {
+  console.log(logSymbols.info, chalk.blue(msg));
+};
+
 exports.warn = function(msg) {
   console.log(logSymbols.warning, chalk.yellow(msg));
 };
